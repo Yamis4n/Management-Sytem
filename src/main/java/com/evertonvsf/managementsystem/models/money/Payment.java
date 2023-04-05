d64b3dbc5b04e45980846c823b5124eb2e9b46bb
Yamis4n <evertonvsf2@gmail.com>
2023-04-05 15:42:52 -0300

    :sparkles: feat: Payment model

---
+++ src/main/java/com/evertonvsf/managementsystem/models/money/Payment.java
@@ -0,0 +1,47 @@
+package com.evertonvsf.managementsystem.models.money;
+
+public class Payment {
+
+    int id;
+    int invoiceId;
+    String paymentWay;
+    double value;
+
+    public Payment(int invoiceId, String paymentWay, double value) {
+        this.invoiceId = invoiceId;
+        this.paymentWay = paymentWay;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(int invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public String getPaymentWay() {
+        return paymentWay;
+    }
+
+    public void setPaymentWay(String paymentWay) {
+        this.paymentWay = paymentWay;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
