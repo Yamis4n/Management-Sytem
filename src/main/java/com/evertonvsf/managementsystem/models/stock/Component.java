d2c6d83dc4beda23c7b631b719b4ca2ba67973a1
Yamis4n <evertonvsf2@gmail.com>
2023-04-05 14:45:44 -0300

    :sparkles: feat: Component model

---
+++ src/main/java/com/evertonvsf/managementsystem/models/stock/Component.java
@@ -0,0 +1,40 @@
+package com.evertonvsf.managementsystem.models.stock;
+
+public class Component {
+    int id; // enumerate
+    int quantity;
+    String description;
+    double value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
