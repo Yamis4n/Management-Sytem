87ddc86e827c1cdf6d3e91e64af023b507bfa887
Yamis4n <evertonvsf2@gmail.com>
2023-04-06 20:50:42 -0300

    :sparkles: feat(dao): List implementation of Client persistence

---
+++ src/main/java/com/evertonvsf/managementsystem/dao/users/client/ClientListImpl.java
@@ -0,0 +1,96 @@
+package com.evertonvsf.managementsystem.dao.users.client;
+
+import com.evertonvsf.managementsystem.models.users.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ClientListImpl implements ClientCRUD{
+    private List<Client> clients;
+    private int newId;
+    ClientListImpl(){
+        this.clients = new ArrayList<Client>();
+        this.newId = 0;
+    }
+    @Override
+    public Client create(Client client) {
+        client.setId(this.newId);
+        this.newId++;
+        this.clients.add(client);
+        return client;
+    }
+
+    @Override
+    public List<Client> findMany() {
+        return new ArrayList<Client>(this.clients);
+    }
+
+    @Override
+    public boolean update(Client client) {
+        for (int index = 0; index < this.clients.size(); index++){
+            if (this.clients.get(index).getId() == client.getId()){
+                this.clients.set(index, client);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteMany() {
+        if (this.clients.size() > 0) {
+            this.clients = new ArrayList<Client>();
+            this.newId = 0;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<Client> findByName(String name) {
+        List<Client> sameNameClients = new ArrayList<Client>();
+        for (Client client : this.clients){
+            if (Objects.equals(client.getName(), name)){
+                sameNameClients.add(client);
+            }
+        }
+        return sameNameClients;
+    }
+
+    @Override
+    public Client findById(int id) {
+        for (Client client : this.clients){
+            if (client.getId() == id){
+                return client;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        for (int index = 0; index < this.clients.size(); index++){
+            if (this.clients.get(index).getId() == id){
+                this.clients.remove(index);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteByName(String name) {
+        List<Client> sameNameClients = new ArrayList<Client>();
+        for (Client client : this.clients) {
+            if (Objects.equals(client.getName(), name)) {
+                sameNameClients.add(client);
+            }
+        }
+        if (sameNameClients.size() > 0){
+            this.clients.removeAll(sameNameClients);
+            return true;
+        }
+        return false;
+    }
+}
