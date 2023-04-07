a3a6a57d284cd38c46b9793660737334d096915b
Yamis4n <evertonvsf2@gmail.com>
2023-04-07 09:14:03 -0300

    :recycle: refactor: Using an User CRUD intereface

---
+++ src/main/java/com/evertonvsf/managementsystem/dao/users/UserCRUD.java
@@ -0,0 +1,13 @@
+package com.evertonvsf.managementsystem.dao.users;
+
+import com.evertonvsf.managementsystem.dao.CRUD;
+import com.evertonvsf.managementsystem.models.users.Client;
+
+import java.util.List;
+
+public interface UserCRUD<Generic> extends CRUD<Generic> {
+    public List<Generic> findByName(String name);
+    public Generic findById(int id);
+    public boolean deleteById(int id);
+    public boolean deleteByName(String name);
+}
--- src/main/java/com/evertonvsf/managementsystem/dao/users/client/ClientCRUD.java
+++ src/main/java/com/evertonvsf/managementsystem/dao/users/client/ClientCRUD.java
@@ -6,8 +6,5 @@ import com.evertonvsf.managementsystem.models.users.Client;
 import java.util.List;
 
 public interface ClientCRUD extends CRUD<Client> {
-    public List<Client> findByName(String name);
-    public Client findById(int id);
-    public boolean deleteById(int id);
-    public boolean deleteByName(String name);
+    
 }
