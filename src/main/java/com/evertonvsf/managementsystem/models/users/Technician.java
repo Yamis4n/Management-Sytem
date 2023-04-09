bd2f7831f944b5d145903a411ab85fccb7e75341
Yamis4n <evertonvsf2@gmail.com>
2023-04-09 10:36:48 -0300

    :recycle: refactor: Technician model now has a ordersId list

--- src/main/java/com/evertonvsf/managementsystem/models/users/Technician.java
+++ src/main/java/com/evertonvsf/managementsystem/models/users/Technician.java
@@ -1,9 +1,24 @@
 package com.evertonvsf.managementsystem.models.users;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Technician {
     private int id;
     private String name;
     private String password;
+    private List<Integer> OrdersIds;
+
+    public List<Integer> getOrdersIds() {
+        return OrdersIds;
+    }
+
+    public void setOrdersIds(int orderId) {
+        if (this.OrdersIds == null){
+            this.OrdersIds = new ArrayList<Integer>();
+        }
+        this.OrdersIds.add(orderId);
+    }
 
     public Technician(String name, String password) {
         this.name = name;
