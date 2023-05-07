3eacf44b5c201804bd8bf3546c6d6a51deb1a1f7
Yamis4n <evertonvsf2@gmail.com>
2023-05-07 20:12:57 -0300

    :recycle: refactor(DAO): Persistence as a abstract class

--- src/main/java/com/evertonvsf/managementsystem/dao/crud/users/client/ClientCRUD.java
+++ src/main/java/com/evertonvsf/managementsystem/dao/crud/users/client/ClientCRUD.java
@@ -1,6 +1,6 @@
 package com.evertonvsf.managementsystem.dao.crud.users.client;
 
-import com.evertonvsf.managementsystem.dao.crud.CRUD.users.UserCRUD;
+import com.evertonvsf.managementsystem.dao.crud.users.UserCRUD;
 import com.evertonvsf.managementsystem.models.users.Client;
 
 public interface ClientCRUD extends UserCRUD<Client> {
--- src/main/java/com/evertonvsf/managementsystem/dao/persistence/Persistence.java
+++ src/main/java/com/evertonvsf/managementsystem/dao/persistence/Persistence.java
@@ -1,8 +1,34 @@
 package com.evertonvsf.managementsystem.dao.persistence;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public interface Persistence<Generic> {
-    public List<Generic> loadFiles();
-    public boolean writeFiles(List<Generic> files);
+
+public abstract class Persistence<Generic>{
+    private final String fileName = "file.byte";
+
+    public List<Generic> loadFiles() {
+        List<Generic> files;
+        try {
+            ObjectInputStream objectInput = new ObjectInputStream(new FileInputStream(fileName));
+            files = (List<Generic>) objectInput.readObject();
+            objectInput.close();
+        } catch (IOException | ClassNotFoundException e) {
+            return new ArrayList<Generic>();
+        }
+        return files;
+    }
+
+
+    public boolean writeFiles(List<Generic> files) {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName));
+            outputStream.writeObject(files);
+            outputStream.close();
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
 }
