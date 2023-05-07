0d5151f0fb09a39d3b7b0562f2548828da4e102b
Yamis4n <evertonvsf2@gmail.com>
2023-05-07 20:15:51 -0300

    :sparkles: new(DAO): create ClientPersistence

---
+++ src/main/java/com/evertonvsf/managementsystem/dao/persistence/users/client/ClientPersistence.java
@@ -0,0 +1,8 @@
+package com.evertonvsf.managementsystem.dao.persistence.users.client;
+
+import com.evertonvsf.managementsystem.dao.persistence.Persistence;
+import com.evertonvsf.managementsystem.models.users.Client;
+
+public class ClientPersistence extends Persistence<Client> {
+    private final String fileName = "Client.byte";
+}
