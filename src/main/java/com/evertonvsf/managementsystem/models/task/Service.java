4884d406449c76157885b9bf8df494dc59f7b080
Yamis4n <evertonvsf2@gmail.com>
2023-05-25 15:08:58 -0300

    :sparkles: new(models): add Service documentation

--- src/main/java/com/evertonvsf/managementsystem/models/task/Service.java
+++ src/main/java/com/evertonvsf/managementsystem/models/task/Service.java
@@ -3,6 +3,17 @@ package com.evertonvsf.managementsystem.models.task;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * Representa um serviço da assistência técnica
+ * ele possui uma categoria representada pela classe <code>ServiceCategory</code>
+ * e um status representado pela classe <code>Status</code>.
+ * 
+ * @author Everton Vinícius da Silva Ferreira
+ * @version 2.5
+ *
+ * @see <code>ServiceCategory</code>
+ * @see <code>Status</code>
+ */
 public class Service implements Serializable {
     private int id, rating, necessaryComponentId, invoiceId;
     private ServiceCategory category;
@@ -11,6 +22,11 @@ public class Service implements Serializable {
     long timeToConclude;
     private double price;
 
+    /**
+     * Construtor da classe <code>Service</code>
+     *
+     * @param category Numero indicando qual categoria o serviço pertence;
+     */
     public Service(int category) {
         this.setCategory(category);
         this.setStatus(0);
@@ -51,7 +67,11 @@ public class Service implements Serializable {
     public String getCategory() {
         return category.getServiceName();
     }
-
+/**
+ * Define a categoria do <code>Servce</code> com base na enumeração presente em <code>ServiceCategory</code>.
+ *
+ * @param category Número indicando qual categoria o <code>Service</code> pertence;
+ */
     public void setCategory(int category) {
         switch (category){
             case 0 -> this.category = ServiceCategory.MOUNTING_RAM;
@@ -68,7 +88,11 @@ public class Service implements Serializable {
     public String getStatus() {
         return status.getStatusName();
     }
-
+/**
+ * Define o status do <code>Service</code> com base na enumeração presente em <code>Status</code>.
+ *
+ * @param statusId Número indicando qual o <code>Status</code> do <code>Service</code>.
+ */
     public void setStatus(int statusId) {
         switch (statusId){
             case 0 -> this.status = Status.WAITING;
@@ -103,7 +127,11 @@ public class Service implements Serializable {
     public void setPrice() {
         this.price = this.category.getPrice();
     }
-
+/**
+ * Gera uma <code>String</code> contendo as informações do <code>Service</code>.
+ *
+ * @return Informações do <code>Service</code>;
+ */
     @Override
     public String toString() {
         return "Service{" +
