20a17b70d2d9b78d75859af4ce7aa25a9c22a092
Yamis4n <evertonvsf2@gmail.com>
2023-06-11 12:33:36 -0300

    :sparkles: new: working edit and create buttons on clients page

--- src/main/java/com/evertonvsf/managementsystem/controllers/ClientsController.java
+++ src/main/java/com/evertonvsf/managementsystem/controllers/ClientsController.java
@@ -1,9 +1,17 @@
 package com.evertonvsf.managementsystem.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class ClientsController extends MenuController{
 
@@ -16,6 +24,29 @@ public class ClientsController extends MenuController{
 
 
     }
+    @FXML
+    private void gotoRegister() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/clients_register.fxml")));
+        popUp(root);
 
-   
+    }
+
+    @FXML
+    private void gotoEdit() throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/clients_edit.fxml")));
+        popUp(root);
+    }
+
+    @FXML
+    private void deleteClient(){}
+
+    public void popUp(Parent root){
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(MainController.stage);
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+
+        stage.show();
+    }
 }
